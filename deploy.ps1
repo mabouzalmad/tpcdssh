5a144e1b23df6ebf98ee2241af93d378890bea49
manal <manal123@gmail.com>
2026-03-30 12:06:45 +0100

    commit

--- deploy.ps1
+++ deploy.ps1
@@ -2,8 +2,8 @@ param(
     [string]$DockerUser,
     [string]$DockerToken
     )
-docker login -u $DockerUser -p $DockerToken
+docker login -u $manal456 -p $DockerToken
 docker stop flask-app -ErrorAction SilentlyContinue
 docker rm flask-app -ErrorAction SilentlyContinue
-docker pull $DockerUser/flask_devops_demo:latest
-docker run -d  --name flask-app  5000:5000 $DockerUser/flask_devops_demo:latest
+docker pull $manal456/flask_devops_demo:latest
+docker run -d  --name flask-app  5000:5000 $manal456/flask_devops_demo:latest
